fa7da5c820364bf4a00354605b61a2861a8dfc7a
Yoav David <yoavv.david2@gmail.com>
2025-01-26 23:16:14 +0200

    Added model & student type

---
+++ app/src/main/java/com/example/androidassignment/model/Student.kt
@@ -0,0 +1,9 @@
+package com.example.androidassignment.model
+
+data class Student(
+    var name: String,
+    var id: String,
+    var phone: String,
+    var address: String,
+    var isChecked: Boolean,
+)
